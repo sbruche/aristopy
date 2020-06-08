611d88c5d86e26b6cbf57c3ce0cd7015f4400b5a
Stefan Bruche <stefan.bruche@tu-berlin.de>
2020-06-08 13:33:58 +0200

    updated documentation of Conversion class

--- aristopy/conversion.py
+++ aristopy/conversion.py
@@ -35,13 +35,20 @@ class Conversion(Component):
         *See the documentation of the Component class for a description of all
         keyword arguments and inherited methods.*
 
-        :param start_up_cost:
+        :param start_up_cost: Costs incurred when the state of the binary
+            operation variable (BI_OP) changes from 0 (OFF) to 1 (ON) from one
+            time step to the next one [€/Start]; (requires keyword argument
+            'has_operation_binary_var' set to True).
             |br| *Default: 0*
-        :type start_up_cost:
+        :type start_up_cost: float or int (>=0)
 
-        :param min_load_rel:
+        :param min_load_rel: Value for the relative minimal part-load of a
+            conversion unit (e.g., 0.5 represents 50% minimal load). Minimal
+            part-loads require the availability of a binary operation variable
+            ('has_operation_binary_var'=True) and currently, fixed capacities
+            need to be specified ('capacity_min'='capacity_max').
             |br| *Default: None*
-        :type min_load_rel:
+        :type min_load_rel: float or int (0<=value<=1), or None
 
         :param instances_in_group: States the number of similar component
             instances that are simultaneously created and arranged in a group.
@@ -55,15 +62,37 @@ class Conversion(Component):
             |br| *Default: 1*
         :type instances_in_group: int (>0)
 
-        :param group_has_existence_order:
+        :param group_has_existence_order: If multiple similar instances are
+            created and arranged in a group ('instances_in_group'>1), the user
+            might want to introduce an order of their binary existence variable
+            values to break the symmetry of the optimization problem. If the
+            flag is set to True a constraint is created for each component of
+            the group that requires the previous component in the group to exist
+            (BI_EX=1) before the own existence variable can take the value 1.
             |br| *Default: True*
         :type group_has_existence_order: bool
 
-        :param group_has_operation_order:
+        :param group_has_operation_order: If multiple similar instances are
+            created and arranged in a group ('instances_in_group'>1), the user
+            might want to introduce an order of their binary operation variable
+            values to break the symmetry of the optimization problem. If the
+            flag is set to True a constraint is created for each component of
+            the group and every time step of the optimization problem that
+            requires the previous component in the group to be operated
+            (BI_OP=1) before the own operation variable can take the value 1.
             |br| *Default: True*
         :type group_has_operation_order: bool
 
-        :param use_inter_period_formulation:
+        :param use_inter_period_formulation: A second binary start-up variable
+            is created (BI_SU_INTER), if the inter-period model formulation is
+            requested (flag=True) and start-up cost are introduced (argument
+            'start_up_cost'>0). This variable is used in case the model is
+            optimized with aggregated time-series data (multiple periods). The
+            variable links the binary operation variable values of otherwise
+            independent periods, to enforce start-up cost if the operation
+            status (BI_OP) changes from one period to the next (OFF to ON).
+            Note: This formulation introduces additional variables and
+            constraints, and increases both model accuracy and model complexity.
             |br| *Default: True*
         :type use_inter_period_formulation: bool
         """
@@ -101,13 +130,22 @@ class Conversion(Component):
             assert min_load_rel <= 1, 'Maximal value for "min_load_rel" is 1!'
             if not self.has_bi_op:
                 raise ValueError('Minimal part-loads require the availability '
-                                 'of a operation binary variable '
+                                 'of a binary operation variable '
                                  '("has_operation_binary_var=True").')
+            if self.capacity is None:
+                raise NotImplementedError(
+                    'Minimal part-loads with flexible unit capacity can be '
+                    'modelled but it requires some effort. This feature has'
+                    ' not been implemented yet. Please consider the '
+                    '"user_expressions" attribute to model it yourself.\n '
+                    'E.g.: Q_CAP_OR_OFF[p, t] == CAP * BI_OP[p, t] * dt '
+                    '--> Linearization required (Glover)!,\n '
+                    'Q[p, t] >= Q_CAP_OR_OFF[p, t] * min_load_rel * dt')
         self.min_load_rel = min_load_rel
 
-        # Check and set more conversion specific input arguments
+        # Check and set additional input arguments
         self.start_up_cost = utils.set_if_positive(start_up_cost)  # [€/Start]
-        utils.is_boolean(use_inter_period_formulation)  # check input
+        utils.is_boolean(use_inter_period_formulation)
         self.use_inter_period_formulation = use_inter_period_formulation
 
         # Multiple instances formed and collected in one group
@@ -169,19 +207,25 @@ class Conversion(Component):
         # Time-dependent constraints :
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         self.con_bi_var_ex_and_op_relation(model)
-        self.con_operation_limit(model)
         self.con_couple_op_binary_and_basic_var(model)
-        self.con_min_load_rel(model)
-        self.con_start_up_cost(ensys, model)
-        self.con_start_up_cost_inter(ensys, model)
+        self.con_operation_limit(model)
         self.con_operation_sym_break(ensys, model)
+        self.con_min_load_rel(model)
+        self.con_start_up_cost(model)
+        self.con_start_up_cost_inter(ensys, model)
 
     # **************************************************************************
     #    Time-independent constraints
     # **************************************************************************
     def con_existence_sym_break(self, ensys):
         """
-        TODO: Add description!
+        Constraint to state, that the next component in a group can only be
+        built if the previous one already exists (symmetry break constraint for
+        groups consisting of multiple components 'instances_in_group' > 1), if
+        keyword argument 'group_has_existence_order' is set to True.
+        E.g.: |br| ``BI_EX (of conversion_2) <= BI_EX (of conversion_1)``
+
+        *Method is not intended for public access!*
         """
         if self.number_in_group > 1 and self.group_has_existence_order:
             bi_ex = self.variables[utils.BI_EX]['pyomo']
@@ -200,10 +244,15 @@ class Conversion(Component):
     # **************************************************************************
     def con_operation_sym_break(self, ensys, model):
         """
-        TODO: Add description!
+        Constraint to state, that the next component in a group can only be
+        operated if the previous one is already 'ON' (symmetry break constraint
+        for groups consisting of multiple components 'instances_in_group' > 1),
+        if keyword argument 'group_has_operation_order' is set to True. E.g.:
+        |br| ``BI_OP[p, t] (of conversion_2) <= BI_OP[p, t] (of conversion_1)``
+
+        *Method is not intended for public access!*
         """
         if self.number_in_group > 1 and self.group_has_operation_order:
-            # Get variables:
             bi_op = self.variables[utils.BI_OP]['pyomo']
             prior = self.group_name + '_{}'.format(self.number_in_group - 1)
             prior_comp = ensys.components[prior]
@@ -216,14 +265,16 @@ class Conversion(Component):
 
     def con_operation_limit(self, model):
         """
-        The operation variable (ref.: basic commodity) of a conversion unit
-        (MWh) is limit by its nominal power (MW) multiplied with the number of
+        The basic variable of a component is limited by its nominal capacity.
+        This usually means, the operation (main commodity) of a conversion
+        (MWh) is limited by its nominal power (MW) multiplied with the number of
         hours per time step. E.g.: |br|
         ``Q[p, t] <= CAP * dt``
+
+        *Method is not intended for public access!*
         """
         # Only required if component has a capacity variable
         if self.has_capacity_var:
-            # Get variables:
             cap = self.variables[utils.CAP]['pyomo']
             basic_var = self.variables[self.basic_variable]['pyomo']
             has_time_set = self.variables[self.basic_variable]['has_time_set']
@@ -233,6 +284,7 @@ class Conversion(Component):
                 if has_time_set:
                     return basic_var[p, t] <= cap * dt
                 else:
+                    # Exceptional case: Selection of a scalar basic variable
                     return basic_var <= cap
 
             setattr(self.block, 'con_operation_limit', pyomo.Constraint(
@@ -240,50 +292,45 @@ class Conversion(Component):
 
     def con_min_load_rel(self, model):
         """
-        Currently minimal part-loads can only be used if a binary variable for
-        operation is defined and fixed capacities of the conversion units are
-        specified (cap_min = cap_max). E.g.: |br|
+        Constraint to set a value for the relative minimal part-load of a
+        conversion unit (e.g., a component can either be switched off (BI_OP=0)
+        or can work between 50% and 100% of its nominal capacity if a value of
+        0.5 for 'min_load_rel' is specified. The constraint requires the
+        availability of a binary operation variable ('has_operation_binary_var'
+        is True) and currently, fixed capacities need to be specified
+        ('capacity_min'='capacity_max'='capacity'). E.g.: |br|
         ``Q[p, t] >= capacity * BI_OP[p, t] * min_load_rel * dt``
+
+        *Method is not intended for public access!*
         """
         # Only required if minimal part-loads should be modelled.
+        # Availability of binary operation variable and fixed capacity is
+        # checked during component initialization.
         if self.min_load_rel is not None:
-            if self.capacity is not None:  # fixed capacity!
+            bi_op = self.variables['BI_OP']['pyomo']
+            basic_var = self.variables[self.basic_variable]['pyomo']
+            cap = self.capacity
+            min_load = self.min_load_rel
+            dt = self.ensys.hours_per_time_step
 
-                bi_op = self.variables['BI_OP']['pyomo']
-                basic_var = self.variables[self.basic_variable]['pyomo']
-                cap = self.capacity
-                min_load = self.min_load_rel
-                dt = self.ensys.hours_per_time_step
+            def con_min_load_rel(m, p, t):
+                return basic_var[p, t] >= cap * bi_op[p, t] * min_load * dt
+            setattr(self.block, 'con_min_load_rel', pyomo.Constraint(
+                model.time_set, rule=con_min_load_rel))
 
-                def con_min_load_rel(m, p, t):
-                    return \
-                        basic_var[p, t] >= cap * bi_op[p, t] * min_load * dt
-
-                setattr(self.block, 'con_min_load_rel', pyomo.Constraint(
-                    model.time_set, rule=con_min_load_rel))
-            else:
-                # TODO: Add this to the init check!
-                raise NotImplementedError(
-                    'Minimal part-loads with flexible unit capacity can be '
-                    'modelled but it requires some effort. This feature has'
-                    ' not been implemented yet. Please consider the '
-                    '"user_expressions" attribute to model it yourself.\n '
-                    'E.g.: Q_CAP_OR_OFF[p, t] == CAP * BI_OP[p, t] * dt '
-                    '--> Linearization required (Glover)!,\n '
-                    'Q[p, t] >= Q_CAP_OR_OFF[p, t] * min_load_rel * dt')
-
-    def con_start_up_cost(self, ensys, model):
+    def con_start_up_cost(self, model):
         """
         Constraint to determine the status of the binary start-up variable. If
-        the operational status of a component changes from OFF (bi_op=0) to ON
-        (bi_op=1) from one time step to the next, the binary start-up variable
-        must take a value of 1. For shut-down and remaining ON or OFF, status
-        variable can take both values but objective function forces it to be 0.
+        the operational status of a component changes from OFF (BI_OP=0) to ON
+        (BI_OP=1) from one time step to the next, the binary start-up variable
+        must take a value of 1. For shut-down and remaining ON / OFF, the status
+        variable can take both values, but the obj. function forces it to be 0.
         E.g.: |br| ``0 <= BI_OP[t-1] - BI_OP[t] + BI_SU[t]``
+
+        *Method is not intended for public access!*
         """
         # Only if the start-up cost value is larger than 0
         if self.start_up_cost > 0:
-            # Get binary variables:
             bi_op = self.variables[utils.BI_OP]['pyomo']
             bi_su = self.variables[utils.BI_SU]['pyomo']
 
@@ -297,13 +344,21 @@ class Conversion(Component):
 
     def con_start_up_cost_inter(self, ensys, model):
         """
-        Todo: Add description
+        Constraint to link the binary operation variables of consecutive
+        periods, to enforce start-up cost if the operation status (BI_OP)
+        changes from one period to the next (OFF to ON).
+        With this, the binary operation status of the last time step in the
+        previous typical period and the operation status of the first time step
+        in the current typical period are compared. Binary variable
+        'BI_SU_INTER' indicates a potential status change from OFF to ON. E.g.:
+        |br| ``0 <= BI_OP[p_typ-1, t_last] - BI_OP[p_typ, 0] + BI_SU_INTER[p]``
+
+        *Method is not intended for public access!*
         """
         # Only if start-up cost are applied, the inter-period formulation is
         # requested and the data is clustered.
         if self.start_up_cost > 0 and self.use_inter_period_formulation \
                 and ensys.is_data_clustered:
-            # Get binary variables:
             bi_op = self.variables[utils.BI_OP]['pyomo']
             bi_su_inter = self.variables[utils.BI_SU_INTER]['pyomo']
 
